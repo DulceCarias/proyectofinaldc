6190d8c4e0fffc32bf54b9435bb36a21d54764b9
DulceCarias <2020-40093@liceocanadiense.edu.gt>
2021-08-25 12:42:47 -0600

    cambio

---
+++ proser.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="diseño.css">
+
+    <title>Productos Y Servicios</title>
+  </head>
+  <body>
+
+  <div class="container">
+        <div class="btn-group w-100" role="group" aria-label="Basic exaple">
+          <button type="button" class="btn btn-info"><a href="index.html">Inicio</a></button>
+          <button type="button" class="btn btn-success"><a href="tiendas.php">Tiendas</a></button>
+          <button type="button" class="btn btn-danger"><a href="conta.php">Contáctenos</a></button>
+    </div>
+    </div>
+  <br>
+  
+
+  <br><br>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-md-3 col-sm-6">
+        <div class="card" style="width: 17rem;">
+          <img src="img/1.jfif" height="200px" class="card-img-top" alt="...">
+          <div class="card-body">
+              <center>
+            <h5 class="card-title">Comedor De Caboa</h5>
+            <p class="card-text">Precio: Q1,600</p>
+            <div class="card-body">
+            <a class="btn btn-danger"  role="button">Mas Informacion</a>
+            </div>
+      </div>
+    </div>
+  </div>
+  </center>
+  <div class="col-md-3 col-sm-6">
+    <div class="card" style="width: 17rem;">
+      <img src="img/2.jpg" height="200px" class="card-img-top" alt="...">
+      <div class="card-body">
+          <center>
+        <h5 class="card-title">Mesa De Noche</h5>
+        <p class="card-text">Precio: Q800</p>
+        <div class="card-body">
+          <a class="btn btn-dark"  role="button">Mas Informacion</a>
+        </div>
+  </div>
+</div>
+</div>
+</center>
+<div class="col-md-3 col-sm-6">
+  <div class="card"  style="width: 17rem;">
+    <img src="img/3.jpg" height="200px" class="card-img-top" alt="...">
+    <div class="card-body">
+        <center>
+      <h5 class="card-title">Estanteria</h5>
+      <p class="card-text">Precio: Q500</p>
+      <div class="card-body">
+        <a class="btn btn-warning"  role="button">Mas Informacion</a>
+      </div>
+</div>
+</div>
+</div>
+</center>
+<div class="col-md-3 col-sm-6">
+  <div class="card"  style="width: 17rem;">
+    <img src="img/4.jpg" height="200px" class="card-img-top" alt="...">
+    <div class="card-body">
+        <center>
+      <h5 class="card-title">Conjunto De Roperos</h5>
+      <p class="card-text">Precio: Q3,600</p>
+      <div class="card-body">
+        <a class="btn btn-info"  role="button">Mas Informacion</a>
+      </div>
+</div>
+</div>
+</div>
+</center>
+</div>
+</div>
+<br>
+<div class="container mt-3">
+  <div class="row">
+    <div class="col-md-3 col-sm-6">
+      <div class="card" style="width: 17rem;">
+        <img src="img/5.jpg" height="200px" class="" alt="...">
+        <div class="card-body">
+            <center>
+          <h5 class="card-title">Conjunto Para Espejo</h5>
+          <p class="card-text">Precio: Q2,300</p>
+          <div class="card-body">
+            <a class="btn btn-success"  role="button"> Mas Informacion</a>
+          </div>
+    </div>
+  </div>
+</div>
+</center>
+<div class="col-md-3 col-sm-6">
+  <div class="card" style="width: 17rem;">
+    <img src="img/6.jpg" height="200px" class="card-img-top" alt="...">
+    <div class="card-body">
+        <center>
+      <h5 class="card-title">Sofa De Cuero</h5>
+      <p class="card-text">Precio: Q1,200</p>
+      <div class="card-body">
+        <a class="btn btn-warning" role="button">Mas Informacion</a>
+      </div>
+</div>
+</div>
+</div>
+</center>
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="img/7.jpg" height="200px" class="card-img-top" alt="...">
+  <div class="card-body">
+      <center>
+    <h5 class="card-title">Estanteria 2 Piezas</h5>
+    <p class="card-text">Precio: Q800</p>
+    <div class="card-body">
+      <a class="btn btn-dark"  role="button"> Mas Informacion</a>
+    </div>
+</div>
+</div>
+</div>
+</center>
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="img/8.jpg" height="200px" class="card-img-top" alt="...">
+  <div class="card-body">
+      <center>
+    <h5 class="card-title">Ropero y Comoda</h5>
+    <p class="card-text">Precio: Q5,000</p>
+    <div class="card-body">
+      <a class="btn btn-danger" role="button">Mas Informacion</a>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</center>
+<br>
+<div class="container mt-3">
+  <div class="row">
+    <div class="col-md-3 col-sm-6">
+      <div class="card" style="width: 17rem;">
+        <img src="img/9.jpg" height="200px" class="card-img-top" alt="...">
+        <div class="card-body">
+            <center>
+          <h5 class="card-title">Ecritorio Para Computadora</h5>
+          <p class="card-text">Precio: Q1,700</p>
+          <div class="card-body">
+            <a class="btn btn-dark"  role="button">Mas Informacion</a>
+          </div>
+    </div>
+  </div>
+</div>
+</center>
+<div class="col-md-3 col-sm-6">
+  <div class="card" style="width: 17rem;">
+    <img src="img/10.jpg" height="200px" class="card-img-top" alt="...">
+    <div class="card-body">
+        <center>
+      <h5 class="card-title">Comoda</h5>
+      <p class="card-text">Precio: Q1,200</p>
+      <div class="card-body">
+            <a class="btn btn-info"  role="button">Mas Informacion</a>
+      </div>
+</div>
+</div>
+</div>
+</center>
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="img/11.jpg" height="200 px" class="card-img-top" alt="...">
+  <div class="card-body">
+      <center>
+    <h5 class="card-title">Conjunto Cama</h5>
+    <p class="card-text">Precio: Q1,220</p>
+    <div class="card-body">
+      <a class="btn btn-success"  role="button">Mas Informacion</a>
+    </div>
+</div>
+</div>
+</div>
+</center>
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="img/12.jpg" height="200x" class="card-img-top" alt="...">
+  <div class="card-body">
+      <center>
+    <h5 class="card-title">Literas</h5>
+    <p class="card-text">Precio: Q1,510</p>
+    <div class="card-body">
+    <a class="btn btn-dark"  role="button">Mas Informacion</a>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</center>
+<br><br><br>
+<div class="d-grid gap-2">
+  <button class="btn btn-warning"  type="button">PBX: 89644005 - Direcion: 9-60, 6A Avenida, Cdad. de Guatemala  - Carnet:2020-40093 - Nombre: Dulce Maria Carias Orantes</button>
+</div>
+
+
+
+
+
+
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  
+  </body>
+</html>
